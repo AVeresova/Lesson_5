240de44a2ca25378d82a6a8ffea797952248a348
AVeresova <72518746+AVeresova@users.noreply.github.com>
2020-10-27 23:28:44 +0300

    Add files via upload

---
+++ Task_4.py
@@ -0,0 +1,9 @@
+def summary():
+    with open('text_4.txt', 'w+') as file_obj:
+        line = input('Введите цифры через пробел \n')
+        file_obj.writelines(line)
+        my_numb = line.split()
+        print(sum(map(int, my_numb)))
+
+
+summary()
